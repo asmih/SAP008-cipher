f9fc407e09f1b5082a4a3db7455fe5544b9953e6
asmih <michelledearaujo2@gmail.com>
2022-08-04 21:13:34 -0300

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-
 function cifrar(event) {
   event.preventDefault();
-  const texto = document.getElementById("getText").value;
+  const texto = document.getElementById("getText").value.toUpperCase();
   const offset = parseInt(document.getElementById("offset").value);
   document.getElementById("codeText").value = cipher.encode(offset, texto);
 
@@ -15,9 +13,9 @@ cifrarButton.addEventListener("click",(event) => cifrar(event));
 
 function decifrar(event) {
   event.preventDefault();
-  const decifrado = document.getElementById("codeText").value;
+  const texto = document.getElementById("codeText").value;
   const offset = parseInt(document.getElementById("offset").value);
-  document.getElementById("getText").value = cipher.decode(offset, decifrado);
+  document.getElementById("getText").value = cipher.decode(offset, texto);
 
 }
 
